22af23a96282d38d25cd9a73634f9fe711207725
Dane Mancuso <danegmancuso@gmail.com>
2020-06-26 00:12:57 +0000

    added findKey.js

---
+++ findKey.js
@@ -0,0 +1,35 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🚫🚫🚫 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+// --------------------------------------
+
+let results = "";
+
+const findKey = function (obj, callback) {
+  for (let key in obj) {
+    if (callback(obj[key])) {
+      results = key;
+      return results;
+    }
+  }
+  results = undefined;
+  return results;
+};
+
+
+console.log (findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3)) // => "Akaleri"
+
+
+// --------------------------------------
+assertEqual(results, "Akaleri");
