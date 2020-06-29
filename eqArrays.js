f6266a0ebce7aba8f4466d169b6c79da4ecbaa3b
Dane Mancuso <danegmancuso@gmail.com>
2020-06-29 17:41:15 +0000

    added eqArraysTest

--- eqArrays.js
+++ eqArrays.js
@@ -1,6 +1,4 @@
 
-const assertEqual = require('./assertEqual')
-
 
 const eqArrays = function (arr1, arr2) {
 
@@ -16,9 +14,11 @@ const eqArrays = function (arr1, arr2) {
   }
 };
 
-// test cases, should app PASS!
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // =>  true 
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // =>  true 
+// assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+
+// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+
+module.exports = eqArrays;
